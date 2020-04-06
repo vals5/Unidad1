749f70d0a978fc27023ae471e9715054e28303d6
Luciano Donati <me@lucianodonati.com>
2020-04-06 19:22:25 -0300

    Commit Inicial Unidad1 - Resuelto

---
+++ Resuelto/Assets/Scripts/ControladorVehiculo.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControladorVehiculo : MonoBehaviour
+{
+    public float velocidadMovimiento = 15, velocidadDeGiro = 5;
+    //public float velocidadDeGiro = 5;
+    
+    private float inputHorizontal;
+    private float inputVertical;
+    
+    // Y ACA PUEDO ESCRIBIR UN COMENTARIO
+    void Update()
+    {
+        inputHorizontal = Input.GetAxis("Horizontal");
+        inputVertical = Input.GetAxis("Vertical");
+        
+        transform.Translate(inputVertical * 
+                            Vector3.forward * velocidadMovimiento * Time.deltaTime);
+        
+        
+        /*
+         transform.Translate(inputHorizontal *
+                            // Direccion  x    escalable    x en funcion del tiempo
+                            Vector3.right * velocidadDeGiro * Time.deltaTime);
+       */
+        
+        // if(inputHorizontal == 1 || inputHorizontal == -1)
+        if (inputVertical != 0) 
+        {
+            transform.Rotate(Vector3.up,inputHorizontal * velocidadDeGiro * Time.deltaTime);
+        }
+    }
+}
---
+++ Resuelto/Assets/Scripts/Seguimiento.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Seguimiento : MonoBehaviour
+{
+    public Transform transformDelAuto;
+
+    public float distanciaSelfieStick = 6;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(
+            transform.position.x,
+            transform.position.y,
+            transformDelAuto.position.z - distanciaSelfieStick);
+    }
+}
