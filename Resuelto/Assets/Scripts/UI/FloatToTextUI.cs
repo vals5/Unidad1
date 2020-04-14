9efaa17134149b3f77932c5f718a395af9b4eee1
Luciano Donati <me@lucianodonati.com>
2020-04-13 20:26:53 -0400

    Agregadas UI y animaciones en clase.

---
+++ Resuelto/Assets/Scripts/UI/FloatToTextUI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class FloatToTextUI : MonoBehaviour
+{
+    public Text text;
+
+    public void UpdateText(float newValue)
+    {
+        if (newValue < 10)
+        {
+            if (newValue == 0)
+                text.text = "Time's up!";
+            else
+                text.text = newValue.ToString("0.0");
+        }
+    }
+}
