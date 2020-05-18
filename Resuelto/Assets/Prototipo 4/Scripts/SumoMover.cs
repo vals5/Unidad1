4c6e307201c24c9bdba448ed052668f181b5376f
Luciano Donati <me@lucianodonati.com>
2020-05-18 19:05:58 -0300

    El jugador ahora puede moverse en 4 direcciones

---
+++ Resuelto/Assets/Prototipo 4/Scripts/SumoMover.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SumoMover : MonoBehaviour
+{
+    public Rigidbody rb = null;
+
+    public float speed = 5;
+
+    public Vector2 move;
+
+    private void Update()
+    {
+        move.x = Input.GetAxis("Horizontal");
+        move.y = Input.GetAxis("Vertical");
+
+        rb.AddForce(new Vector3(move.x, 0, move.y) * (speed * Time.deltaTime), ForceMode.Force);
+    }
+}
