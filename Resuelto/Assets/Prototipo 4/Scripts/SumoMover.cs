0fd5bea7398b1352bb95007d3cfc2f0cdc5920d2
Luciano Donati <me@lucianodonati.com>
2020-05-18 20:56:58 -0300

    El jugador ahora empuja a los enemigos

--- Resuelto/Assets/Prototipo 4/Scripts/SumoMover.cs
+++ Resuelto/Assets/Prototipo 4/Scripts/SumoMover.cs
@@ -7,12 +7,22 @@ public class SumoMover : MonoBehaviour
 {
     public Rigidbody rb = null;
 
-    public float speed = 5;
+    public float speed = 5, fuerzaEmpuje = 5;
 
     public Transform puntoFocalTransform = null;
 
     private void Update()
     {
         rb.AddForce(puntoFocalTransform.forward * (Input.GetAxis("Vertical") * (speed * Time.deltaTime)));
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Enemigo"))
+        {
+            //                       DEST           -        ORIGEN
+            Vector3 dir = (other.transform.position - transform.position).normalized;
+            other.rigidbody.AddForce(dir * fuerzaEmpuje, ForceMode.Impulse);
+        }
     }
 }
